97d6074f541880cd3d335ab5d809490255c5abd6
DoodMac <48062579+jbird84@users.noreply.github.com>
2023-02-21 10:38:01 -0600

    Initial Commit

---
+++ Instagram-Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram-Clone
+//
+//  Created by Kinney Kare on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
